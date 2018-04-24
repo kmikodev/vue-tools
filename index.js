3c8ab0f2cd7228977ce082f3e5da9e339146b048
Jaime Cardona Villegas <jcardonavillegas@gmail.com>
2018-04-24 12:03:00 +0200

    feat(version)

--- index.js
+++ index.js
@@ -21,70 +21,97 @@ console.log(
 
 let findCount = 5;
 let rootPath = '';
-
+let html = '';
+let properties = {};
 const run = async () => {
-    const options = await inquirer.askGithubCredentials();
-    switch (options.generate) {
+    properties = await inquirer.askGithubCredentials();
+    switch (properties.generate) {
         case 'component':
-            options.componentProperties = await componentFlow();
-            generateComponent(options);
+            properties.componentProperties = await componentFlow();
+            generateComponent();
             break;
     }
 }
 
 componentFlow = async () => {
+    debugger
     let componentProperties = await inquirer.componentGenerate();
     componentProperties.isView = componentProperties.isView === 'yes';
     if (componentProperties.isView) {
         const path = await inquirer.componentRoute();
-        componentProperties.path = path.path;
+        componentProperties = Object.assign(componentProperties, path);;
     }
     const componentFolder = await inquirer.componentFolder();
     componentProperties = Object.assign(componentProperties, componentFolder);
     return componentProperties;
 }
 
-generateComponent = (properties) => {
+generateComponent = () => {
     const template = fs.readFileSync(__dirname + '/templates/_component.html', 'utf8', (err, data) => {
         if (err) throw err;
     });
     const parseProp = properties.componentProperties;
 
-    const html = template
+    html = template
         .replace(/<%= kebabName %>/g, _.kebabCase(parseProp.name))
-        .replace(/<%= camelName %>/g, _.camelCase(parseProp.name))
+        .replace(/<%= camelName %>/g, _.upperFirst(_.camelCase(parseProp.name)))
         .replace(/<%= name %>/g, parseProp.name);
 
-    createComponentFile(html, parseProp);
-    return html;
+    createFiles(parseProp);
 }
 
-createComponentFile = async (html, properties) => {
-   
+createFiles = async () => {
     const filelist = _.without(fs.readdirSync('.'), );
-    findPackageJson('.', properties, html);
-
+    findPackageJson('.', createComponentFile);
+    findPackageJson('.', addRoute);
 }
 
-findPackageJson = async (_path, properties, html) => {
-    console.log( chalk.yellow(
-        figlet.textSync('properties', { horizontalLayout: 'full' })
-    ));
-    console.log(properties)
-    const componentFileName = `${_.upperFirst(_.camelCase(properties.name))}.vue`;    
+createComponentFile = (_path) => {
+    const _prop = properties.componentProperties;
+    const componentFileName = `${_.upperFirst(_.camelCase(_prop.name))}.vue`;
+    if (fs.existsSync(`${_path}/${_prop.folder}`)) {
+        fs.writeFileSync(`${_path}/${_prop.folder}/${componentFileName}`, html);
+    } else {
+        console.error(`${_path}/${_prop.folder} not exist`);
+    }
+}
+
+findPackageJson = async (_path, callback) => {
     const filelist = _.without(fs.readdirSync(_path), );
     if (!findCount || findCount < 0) process.exit(1);
     findCount--;
     if (filelist.indexOf('package.json') !== -1) {
         // find component folder
-        if (fs.existsSync(`${_path}/${properties.folder}`)) {
-            fs.writeFileSync(`${_path}/${properties.folder}/${componentFileName}`, html);
-        }
+        callback(_path);
     }
     else {
         findPackageJson(_path + '/..');
     }
 }
 
+addRoute = async (_path) => {
+    const _prop = properties.componentProperties;
+    if (fs.existsSync(`${_path}/${_prop.routesFile}`)) {
+        var routesContent = fs.readFileSync(`${_path}/${_prop.routesFile}`, 'utf8');
+        var routes = /\[(?:[^\]\['"]+|'[^']*'|"[^"]*")*\]/g.exec(routesContent)[0];
+        const newRoute = `,
+    {
+      path: '/${_.kebabCase(_prop.path)}',
+      name: '${_.upperFirst(_.camelCase(_prop.name))}',
+      component: ${_.upperFirst(_.camelCase(_prop.name))}
+    }
+  `;
+        const position = routes.lastIndexOf(']');
+        var output = [routes.slice(0, position), newRoute, routes.slice(position)].join('');
+        routesContent = routesContent.replace(routes, output);
+
+        routesContent = routesContent.replace('\'vue-router\'', `'vue-router'
+import ${_.upperFirst(_.camelCase(_prop.name))} from '@${_prop.folder.replace('src','')}/${_.upperFirst(_.camelCase(_prop.name))}'`)
+        fs.writeFileSync(`${_path}/${_prop.routesFile}`, routesContent);
+
+    } else {
+        console.error(`${_path}/${_prop.routesFile} not exist`);
+    }
+}
 
 run();
